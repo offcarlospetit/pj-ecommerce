01fcbbeb1a90eb3116773192f7fad9d2f63325a8
Carlos Petit <ca.alberto.p@gmail.com>
2022-11-14 16:40:19 -0300

    wip: refactor Home and Detail containers

--- src/home/containers/Detail.tsx
+++ src/home/containers/Detail.tsx
@@ -1,56 +1,21 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Animated,
-  Dimensions,
-  Easing,
-  ScrollView,
-  Pressable,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
-import React, {FC, useEffect, useMemo} from 'react';
-import {
-  NativeStackNavigationProp,
-  NativeStackScreenProps,
-} from '@react-navigation/native-stack';
-import {RootNavigationParamList} from '../../navigation/RootNavigation';
-import {
-  CompositeNavigationProp,
-  CompositeScreenProps,
-  useNavigation,
-} from '@react-navigation/native';
-import {DetailStackParamList} from '../navigation/DetailStack';
-import {Heaader, pallete, useAnimation, width, height} from '../../ui';
+import React, {FC} from 'react';
+import {View, Animated, Easing, ScrollView, Image} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import Icon from 'react-native-vector-icons/AntDesign';
-import {useDispatch, useSelector} from 'react-redux';
-import {addTocart, removeFromCart} from '../reducers/CartSlice';
-import {RootState} from '../../store';
-import {Product} from '../types';
-import {BottomTabNavigationProp} from '@react-navigation/bottom-tabs';
-import {TabParamList} from '../../navigation/TabNavigation';
-import {CartStackParamList} from '../../cart';
-// import S from '../../i18n';
-// import {RecipetEnum} from '../i18n/keys';
+import {Heaader, useAnimation} from '../../ui';
+import {DetailNavigationProps} from '../navigation/type';
+import DetailToggleButtons from '../components/DetailToggleButtons';
+import useHome from '../hooks/useHome';
+import BodyDetail from '../components/BodyDetail';
+import {DetailStyles as styles} from '../styles/DetailStyles';
+import {section_one, section_two} from '../constants';
 
-type Props = CompositeScreenProps<
-  NativeStackScreenProps<DetailStackParamList, 'Detail'>,
-  NativeStackScreenProps<RootNavigationParamList, 'DetailStack'>
->;
-
-const section_one = height * 0.4;
-const section_two = height * 0.6;
-
-const DetailContainer: FC<Props> = ({navigation, route}) => {
+const DetailContainer: FC<DetailNavigationProps> = ({navigation, route}) => {
   const {item} = route.params;
+  const {isInCart, addToCart, removeItemFromCart} = useHome();
   const animated_section_one = useAnimation();
-  const dispatch = useDispatch();
   const animated_section_two = useAnimation();
-  const [scrollY, setScrollY] = React.useState(0);
-  const {top, bottom} = useSafeAreaInsets();
-  const cartState = useSelector((state: RootState) => state.cart);
+  const [_, setScrollY] = React.useState(0);
+  const {top} = useSafeAreaInsets();
 
   animated_section_two.addListener(({value}) => {
     setScrollY(value);
@@ -86,39 +51,18 @@ const DetailContainer: FC<Props> = ({navigation, route}) => {
     outputRange: [0, 0],
   });
 
-  const isInCart = useMemo(
-    () => (item: string) => {
-      console.log({item, cartState: cartState.products});
-      const quantity = cartState.products.filter(
-        (product: Product) => product.tail === item,
-      ).length;
-
-      return {isInCart: quantity > 0, quantity: quantity};
-    },
-    [cartState],
-  );
-
   return (
     <View>
       <Heaader title={item.amiiboSeries} iconleft />
-      <Animated.View
-        style={{
-          position: 'absolute',
-          width,
-          height,
-        }}>
+      <Animated.View style={styles.mainContainer}>
         <Animated.View
-          style={{
-            width,
-            backgroundColor: pallete.fourth,
-            borderBottomStartRadius: 5,
-            borderBottomEndRadius: 5,
-            position: 'absolute',
-            top: 0,
-            zIndex: 1,
-            height: animated_section_one,
-            transform: [{translateY: interpolated_section_one}],
-          }}>
+          style={[
+            styles.imageContainer,
+            {
+              height: animated_section_one,
+              transform: [{translateY: interpolated_section_one}],
+            },
+          ]}>
           <Image
             source={{uri: item.image}}
             blurRadius={0.5}
@@ -126,234 +70,27 @@ const DetailContainer: FC<Props> = ({navigation, route}) => {
           />
         </Animated.View>
         <Animated.View
-          style={{
-            height: animated_section_two,
-            width,
-            transform: [{translateY: interpolated_section_two}],
-            backgroundColor: 'white',
-            borderTopStartRadius: 20,
-            borderTopEndRadius: 20,
-            position: 'absolute',
-            bottom: 0,
-            zIndex: 2,
-            shadowColor: '#000',
-            shadowOffset: {
-              width: 0,
-              height: 2,
+          style={[
+            styles.detailContainer,
+            {
+              height: animated_section_two,
+              transform: [{translateY: interpolated_section_two}],
             },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-            elevation: 5,
-          }}>
-          <View style={{flex: 1}}>
+          ]}>
+          <View style={styles.scrollContainer}>
             <ScrollView
-              style={{flexGrow: 1}}
+              style={styles.scrollviewStyle}
               scrollEnabled
               showsVerticalScrollIndicator={false}
-              scrollEventThrottle={12}
-              // onScroll={e => {
-              //   if (
-              //     e.nativeEvent.contentOffset.y > 0 &&
-              //     e.nativeEvent.contentOffset.y < top * 3.617
-              //   )
-              //     animated_section_two.setValue(
-              //       section_two + e.nativeEvent.contentOffset.y,
-              //     );
-              // }}
-            >
-              <View style={{flex: 1, paddingBottom: 24}}>
-                <View style={{flex: 1, padding: 16, flexDirection: 'row'}}>
-                  <View style={{flex: 1}}>
-                    <Text
-                      style={{
-                        fontSize: 24,
-                        fontStyle: 'italic',
-                        fontWeight: '800',
-                        color: pallete.secondary,
-                      }}>
-                      {item.amiiboSeries}
-                    </Text>
-                    <Text
-                      style={{
-                        fontSize: 18,
-                        fontWeight: '800',
-                        color: pallete.secondary,
-                      }}>
-                      {item.name}
-                    </Text>
-                  </View>
-                  <View
-                    style={{
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}>
-                    <Text
-                      style={{
-                        fontSize: 24,
-                        fontWeight: '800',
-                        color: pallete.black,
-                      }}>
-                      {item.formatPrice}
-                    </Text>
-                  </View>
-                </View>
-                {/* divider */}
-                <View
-                  style={{
-                    borderWidth: 1,
-                    marginHorizontal: 16,
-                    borderColor: pallete.gray,
-                    marginVertical: 16,
-                  }}
-                />
-                <View
-                  style={{
-                    flex: 1,
-                    flexDirection: 'row',
-                    padding: 8,
-                    paddingLeft: 16,
-                  }}>
-                  <View style={{flex: 1, flexDirection: 'row'}}>
-                    <Text style={{color: pallete.black50, fontWeight: '700'}}>
-                      {'Caracter: '}
-                    </Text>
-                    <Text style={{color: pallete.black, fontWeight: '400'}}>
-                      {item.character}
-                    </Text>
-                  </View>
-                  <View style={{flex: 1, flexDirection: 'row'}}>
-                    <Text style={{color: pallete.black50, fontWeight: '700'}}>
-                      {'Serie : '}
-                    </Text>
-                    <Text style={{color: pallete.black, fontWeight: '400'}}>
-                      {item.gameSeries}
-                    </Text>
-                  </View>
-                </View>
-                <View
-                  style={{
-                    flex: 1,
-                    flexDirection: 'row',
-                    padding: 8,
-                    paddingLeft: 16,
-                  }}>
-                  <View style={{flex: 1, flexDirection: 'row'}}>
-                    <Text style={{color: pallete.black50, fontWeight: '700'}}>
-                      {'Head : '}
-                    </Text>
-                    <Text style={{color: pallete.black, fontWeight: '400'}}>
-                      {item.head}
-                    </Text>
-                  </View>
-                  <View style={{flex: 1, flexDirection: 'row'}}>
-                    <Text style={{color: pallete.black50, fontWeight: '700'}}>
-                      {'Nombre : '}
-                    </Text>
-                    <Text style={{color: pallete.black, fontWeight: '400'}}>
-                      {item.name}
-                    </Text>
-                  </View>
-                </View>
-                <View
-                  style={{
-                    flex: 1,
-                    flexDirection: 'row',
-                    padding: 8,
-                    paddingLeft: 16,
-                  }}>
-                  <View style={{flex: 1, flexDirection: 'row'}}>
-                    <Text style={{color: pallete.black50, fontWeight: '700'}}>
-                      {'Typo : '}
-                    </Text>
-                    <Text style={{color: pallete.black, fontWeight: '400'}}>
-                      {item.type}
-                    </Text>
-                  </View>
-                </View>
-              </View>
+              scrollEventThrottle={12}>
+              <BodyDetail item={item} />
             </ScrollView>
-            <View
-              style={{
-                position: 'absolute',
-                bottom: 0,
-                width,
-                height: (1 / 8) * height,
-                paddingHorizontal: 0,
-                marginBottom: 0,
-              }}>
-              <View
-                style={{
-                  flex: 1,
-                  backgroundColor: pallete.fourth,
-                  padding: 16,
-                  justifyContent: 'flex-start',
-                  alignItems: 'flex-end',
-                  flexDirection: 'row',
-                  shadowColor: '#000',
-                  shadowOffset: {
-                    width: 0,
-                    height: 2,
-                  },
-                  shadowOpacity: 0.25,
-                  shadowRadius: 3.84,
-                  elevation: 5,
-                }}>
-                <View
-                  style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'flex-start',
-                    marginBottom: bottom * 0.9,
-                  }}>
-                  <Text
-                    style={{
-                      fontSize: 22,
-                      fontWeight: '800',
-                      fontStyle: 'italic',
-                      color: pallete.secondary,
-                    }}>
-                    Añadir al carrito
-                  </Text>
-                </View>
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    justifyContent: 'flex-end',
-                    alignItems: 'center',
-                    marginBottom: bottom * 0.9,
-                  }}>
-                  <TouchableOpacity
-                    onPress={() => dispatch(addTocart(item))}
-                    style={{marginRight: 10}}>
-                    <Icon
-                      name="pluscircleo"
-                      size={36}
-                      color={pallete.secondary}
-                    />
-                  </TouchableOpacity>
-                  <Text
-                    style={{
-                      paddingHorizontal: 4,
-                      borderRadius: 5,
-                      borderWidth: 1,
-                      borderColor: '#D6E4E5',
-                    }}>
-                    {isInCart(item.tail).isInCart
-                      ? isInCart(item.tail).quantity
-                      : '0'}
-                  </Text>
-                  <TouchableOpacity
-                    onPress={() => dispatch(removeFromCart(item.tail))}
-                    style={{marginLeft: 10}}>
-                    <Icon
-                      name="minuscircleo"
-                      size={36}
-                      color={pallete.primary}
-                    />
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </View>
+            <DetailToggleButtons
+              add={() => addToCart(item)}
+              remove={() => removeItemFromCart(item.tail)}
+              quantity={isInCart(item.tail).quantity}
+              isInCart={isInCart(item.tail).isInCart}
+            />
           </View>
         </Animated.View>
       </Animated.View>
@@ -362,13 +99,3 @@ const DetailContainer: FC<Props> = ({navigation, route}) => {
 };
 
 export default DetailContainer;
-
-const styles = StyleSheet.create({
-  image: {
-    width: '70%',
-    height: '70%',
-    resizeMode: 'contain',
-    position: 'absolute',
-    alignSelf: 'center',
-  },
-});
--- src/home/containers/Home.tsx
+++ src/home/containers/Home.tsx
@@ -1,179 +1,93 @@
-import {
-  ActivityIndicator,
-  FlatList,
-  Image,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import React, {FC, useEffect, useMemo} from 'react';
-import {useGetProductsQuery} from '../hooks';
-import Icon from 'react-native-vector-icons/AntDesign';
-import {addTocart, removeFromCart} from '../reducers/CartSlice';
-import TouchableScale from 'react-native-touchable-scale';
-import {useDispatch, useSelector} from 'react-redux';
-import {Product} from '../types';
-import {RootState} from '../../store';
-import {CompositeScreenProps} from '@react-navigation/native';
-import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
-import {TabParamList} from '../../navigation/TabNavigation';
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {DetailStackParamList} from '../navigation/DetailStack';
-import {RootNavigationParamList} from '../../navigation/RootNavigation';
-import {Container, Heaader} from '../../ui';
+import React, {FC} from 'react';
+import {FlatList, Image, StatusBar, Text, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-
-type HomeScreenNavigationProp = CompositeScreenProps<
-  BottomTabScreenProps<TabParamList, 'HomeTab'>,
-  CompositeScreenProps<
-    NativeStackScreenProps<DetailStackParamList, 'Detail'>,
-    NativeStackScreenProps<RootNavigationParamList, 'DetailStack'>
-  >
->;
+import TouchableScale from 'react-native-touchable-scale';
+import {Product} from '../types';
+import {Container, Heaader} from '../../ui';
+import {HomeScreenNavigationProp} from '../navigation/type';
+import S from '../../i18n';
+import useHome from '../hooks/useHome';
+import {HomeStyles as styles} from '../styles/HomeStyles';
+import transformPrice from '../../utils';
+import Footer from '../components/Footer';
+import ToggleButtons from '../components/ToggleButtons';
+import Loader from '../components/Loader';
 
 const Home: FC<HomeScreenNavigationProp> = ({navigation}) => {
-  const {data, refetch} = useGetProductsQuery({});
+  const {
+    memoizedData,
+    loading,
+    isInCart,
+    fakeCall,
+    addToCart,
+    removeItemFromCart,
+    isLoading,
+  } = useHome();
   const {top} = useSafeAreaInsets();
-  const dispatch = useDispatch();
-  const [page, setPage] = React.useState(1);
-  const [loading, setLoading] = React.useState(false);
-  const cartState = useSelector((state: RootState) => state.cart);
 
-  const memoizedData = useMemo(() => {
-    let preparedData;
-    if (data) {
-      preparedData = data.amiibo.slice(0, 10 * page);
-    }
-    return preparedData;
-  }, [data, page]);
+  const footerRender = () => {
+    return loading ? <Footer loading={loading} /> : null;
+  };
 
-  const isInCart = useMemo(
-    () => (item: string) => {
-      const quantity = cartState.products.filter(
-        (product: Product) => product.tail === item,
-      ).length;
+  const navigateToDetail = (item: Product) =>
+    navigation.navigate('DetailStack', {
+      screen: 'Detail',
+      params: {item},
+    });
 
-      return {isInCart: quantity > 0, quantity: quantity};
-    },
-    [cartState, page],
-  );
-
-  const fakeCall = () => {
-    setLoading(true);
-    setTimeout(() => {
-      setPage(page + 1);
-      setLoading(false);
-    }, 1500);
+  const renderItem = ({item}: {item: Product}) => {
+    return (
+      <TouchableScale
+        activeScale={0.9}
+        tension={20}
+        friction={7}
+        useNativeDriver
+        style={styles.base}
+        onPress={() => navigateToDetail(item)}>
+        <View style={styles.container}>
+          <View style={styles.imageContainer}>
+            <Image
+              source={{uri: item.image}}
+              style={styles.image}
+              resizeMode="contain"
+            />
+          </View>
+          <View style={styles.detail}>
+            <Text>{item.name}</Text>
+            <Text>{transformPrice(item.price)}</Text>
+          </View>
+          <ToggleButtons
+            add={() => addToCart(item)}
+            remove={() => removeItemFromCart(item.tail)}
+            quantity={isInCart(item.tail).quantity}
+            isInCart={isInCart(item.tail).isInCart}
+          />
+        </View>
+      </TouchableScale>
+    );
   };
 
   return (
     <Container>
       <StatusBar hidden />
-      <Heaader title="Home" />
+      <Heaader title={S.Home.home} />
       <View style={{marginTop: top}}>
-        <FlatList
-          numColumns={2}
-          extraData={memoizedData}
-          data={memoizedData}
-          onEndReached={fakeCall}
-          onEndReachedThreshold={0.1}
-          ListFooterComponent={() => {
-            return loading ? (
-              <View style={styles.loader}>
-                <ActivityIndicator size="large" color="#0000ff" />
-              </View>
-            ) : null;
-          }}
-          renderItem={({item}: {item: Product}) => {
-            return (
-              // product view card
-              <TouchableScale
-                activeScale={0.9}
-                tension={20}
-                friction={7}
-                useNativeDriver
-                style={{flex: 1}}
-                onPress={() =>
-                  navigation.navigate('DetailStack', {
-                    screen: 'Detail',
-                    params: {item},
-                  })
-                }>
-                <View
-                  style={{
-                    flex: 1,
-                    backgroundColor: 'white',
-                    margin: 10,
-                    padding: 10,
-                    borderRadius: 10,
-                    shadowColor: '#000',
-                    shadowOffset: {
-                      width: 0,
-                      height: 2,
-                    },
-                    shadowOpacity: 0.25,
-                    shadowRadius: 3.84,
-                    elevation: 5,
-                  }}>
-                  <View style={{flex: 1}}>
-                    <Image
-                      source={{uri: item.image}}
-                      style={{width: 100, height: 100}}
-                      resizeMode="contain"
-                    />
-                  </View>
-                  <View style={{flex: 2}}>
-                    <Text>{item.name}</Text>
-                    <Text>{item.formatPrice}</Text>
-                  </View>
-                  <View
-                    style={{
-                      flex: 1,
-                      flexDirection: 'row',
-                      paddingVertical: 10,
-                      justifyContent: 'flex-end',
-                      alignItems: 'center',
-                    }}>
-                    <TouchableOpacity
-                      onPress={() => dispatch(addTocart(item))}
-                      style={{marginRight: 10}}>
-                      <Icon name="pluscircleo" size={18} color={'#497174'} />
-                    </TouchableOpacity>
-                    <Text
-                      style={{
-                        paddingHorizontal: 4,
-                        borderRadius: 5,
-                        borderWidth: 1,
-                        borderColor: '#D6E4E5',
-                      }}>
-                      {isInCart(item.tail).isInCart
-                        ? isInCart(item.tail).quantity
-                        : '0'}
-                    </Text>
-                    <TouchableOpacity
-                      onPress={() => dispatch(removeFromCart(item.tail))}
-                      style={{marginLeft: 10}}>
-                      <Icon name="minuscircleo" size={18} color={'#EB6440'} />
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </TouchableScale>
-            );
-          }}
-        />
+        {isLoading ? (
+          <Loader loading={isLoading} />
+        ) : (
+          <FlatList
+            numColumns={2}
+            extraData={memoizedData}
+            data={memoizedData}
+            onEndReached={fakeCall}
+            onEndReachedThreshold={0.1}
+            ListFooterComponent={footerRender}
+            renderItem={renderItem}
+          />
+        )}
       </View>
     </Container>
   );
 };
 
 export default Home;
-
-const styles = StyleSheet.create({
-  loader: {
-    marginTop: 10,
-    alignItems: 'center',
-  },
-});
