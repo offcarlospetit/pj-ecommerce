96bfc12f5c1fa0ffba4c324a64b308e05999f275
Carlos Petit <ca.alberto.p@gmail.com>
2022-11-13 23:12:35 -0300

    wip: update root app entry points and new navigation struct

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,19 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {Provider} from 'react-redux';
 import {store, persistor} from './store';
-import {Home} from './home';
+import NavigationApp from './App';
 import {PersistGate} from 'redux-persist/integration/react';
 
 type Props = {};
 
-const App = (props: Props) => {
+const RootApp = (props: Props) => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Home />
+        <NavigationApp />
       </PersistGate>
     </Provider>
   );
 };
 
-export default App;
+export default RootApp;
