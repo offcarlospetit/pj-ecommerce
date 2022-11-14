ea107eff2c4715251fca70afe118742ca559d4e5
Carlos Petit <ca.alberto.p@gmail.com>
2022-11-14 16:29:32 -0300

    wip: add expo libraries

--- src/cart/containers/Checkout.tsx
+++ src/cart/containers/Checkout.tsx
@@ -7,18 +7,21 @@ import {
   View,
 } from 'react-native';
 import React, {FC, useMemo} from 'react';
-import {useSelector} from 'react-redux';
 import {useNavigation} from '@react-navigation/native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {RootState} from '../../store';
 import {Container, Heaader, height, pallete, width} from '../../ui';
+import {NavigationProps} from '../navigation/types';
+import S from '../../i18n';
+import {FAKE_ADDRESS, TOP} from '../constants';
+import useHome from '../../home/hooks/useHome';
+import transformPrice from '../../utils';
 
 type Props = {};
 
-const Checkout: FC<Props> = (props: Props) => {
-  const cartState = useSelector((state: RootState) => state.cart);
-  const navigation = useNavigation();
-  const {top, bottom} = useSafeAreaInsets();
+const Checkout: FC<Props> = ({}) => {
+  const {cartState} = useHome();
+  const navigation = useNavigation<NavigationProps>();
+  const {top} = useSafeAreaInsets();
   const totalPrice = useMemo(() => {
     const totalPriceValue = cartState.products.reduce((acc: any, item) => {
       return acc + item.price;
@@ -31,62 +34,53 @@ const Checkout: FC<Props> = (props: Props) => {
 
   return (
     <Container>
-      <Heaader title="Tu carrito" iconleft animated={false} iconright={false} />
+      <Heaader
+        title={S.Cart.checkout}
+        iconleft
+        animated={false}
+        iconright={false}
+      />
       <ScrollView nestedScrollEnabled showsVerticalScrollIndicator={false}>
-        <View style={{flex: 1, top: top + 35, paddingHorizontal: 16}}>
-          <View style={{flex: 1, marginTop: 16}}>
-            <View
-              style={{
-                justifyContent: 'flex-start',
-                alignItems: 'flex-start',
-              }}>
-              <Text style={styles.cartTotalText}>Direccion de envio</Text>
+        <View style={[styles.container, {top: top + TOP}]}>
+          <View style={styles.addressContainer}>
+            <View style={styles.address}>
+              <Text style={styles.cartTotalText}>{S.Checkout.address}</Text>
             </View>
-            <View style={{flex: 1, flexDirection: 'row'}}>
-              <View style={{flex: 1}}>
-                <Text style={styles.totalText}>Avenida Beauchef 1325</Text>
+            <View style={styles.addressLine}>
+              <View style={styles.base}>
+                <Text style={styles.totalText}>{FAKE_ADDRESS}</Text>
               </View>
             </View>
           </View>
           <View style={[styles.cartItems]}>
-            <Text style={styles.cartItemsText}>Resumen del carrito</Text>
+            <Text style={styles.cartItemsText}>{S.Checkout.cart_resume}</Text>
             {cartState.products.map(item => {
               return (
                 <View
-                  style={{
-                    padding: 8,
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    marginVertical: 8,
-                  }}
-                  key={item.tail}>
+                  style={styles.productsContainer}
+                  key={item.tail + Math.random()}>
                   <View>
                     <Image source={{uri: item.image}} style={styles.image} />
                   </View>
-                  <View style={{flex: 1}}>
+                  <View style={styles.base}>
                     <Text>{item.name}</Text>
                   </View>
                   <View>
-                    <Text>{item.formatPrice}</Text>
+                    <Text>{transformPrice(item.price)}</Text>
                   </View>
                 </View>
               );
             })}
           </View>
-          <View style={{flex: 1, marginTop: 16}}>
-            <View
-              style={{
-                justifyContent: 'flex-start',
-                alignItems: 'flex-start',
-              }}>
-              <Text style={styles.cartTotalText}>Resumen de tu compra</Text>
+          <View style={styles.resumeContainer}>
+            <View style={styles.resumeTitle}>
+              <Text style={styles.cartTotalText}>{S.Cart.shop_resume}</Text>
             </View>
-            <View style={{flex: 1, flexDirection: 'row'}}>
-              <View style={{flex: 1}}>
-                <Text style={styles.totalText}>Total</Text>
+            <View style={styles.resumeTotalContainer}>
+              <View style={styles.base}>
+                <Text style={styles.totalText}>{S.Cart.total}</Text>
               </View>
-              <View style={{flex: 1, alignItems: 'flex-end'}}>
+              <View style={styles.totalPrice}>
                 <Text style={styles.cartItemsText}>{totalPrice}</Text>
               </View>
             </View>
@@ -94,14 +88,16 @@ const Checkout: FC<Props> = (props: Props) => {
         </View>
       </ScrollView>
       <View style={styles.cartTotal}>
-        <View
-          style={{
-            flex: 1,
-            paddingHorizontal: 24,
-          }}>
-          <TouchableOpacity style={{flex: 1}}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate('CartStack', {
+                screen: 'Receipt',
+              })
+            }
+            style={styles.base}>
             <View style={styles.button}>
-              <Text style={styles.buttonText}>Pagar</Text>
+              <Text style={styles.buttonText}>{S.Cart.continue_shopping}</Text>
             </View>
           </TouchableOpacity>
         </View>
@@ -113,10 +109,36 @@ const Checkout: FC<Props> = (props: Props) => {
 export default Checkout;
 
 const styles = StyleSheet.create({
+  base: {
+    flex: 1,
+  },
+  addressContainer: {
+    flex: 1,
+    marginTop: 16,
+  },
+  address: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  addressLine: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
   goBack: {
     justifyContent: 'center',
     alignItems: 'center',
     padding: 8,
+  },
+  productsContainer: {
+    padding: 8,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 8,
   },
   title: {
     fontSize: 24,
@@ -172,6 +194,10 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: 'bold',
   },
+  buttonContainer: {
+    flex: 1,
+    paddingHorizontal: 24,
+  },
   button: {
     backgroundColor: pallete.primary,
     padding: 16,
@@ -183,4 +209,11 @@ const styles = StyleSheet.create({
     color: pallete.white,
     fontWeight: 'bold',
   },
+  resumeContainer: {flex: 1, marginTop: 16},
+  resumeTitle: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  resumeTotalContainer: {flex: 1, flexDirection: 'row'},
+  totalPrice: {flex: 1, alignItems: 'flex-end'},
 });
